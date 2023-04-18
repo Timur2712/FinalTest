c93b2bb8ab6b51eaa86a628a901e4e01f0db9a32
Timur <matros2727@mail.ru>
2023-04-18 14:13:18 +0300

    Добавление кода

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿string[] array1 = new string[5] { "Mon", "2", "Wens", "Thursday", "Fri" };
+string[] CopyArray(string[] array1)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] array2 = new string[count];
+    count = 0;
+
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+    return array2;
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+string[] array2 = CopyArray(array1);
+PrintArray(array2);
